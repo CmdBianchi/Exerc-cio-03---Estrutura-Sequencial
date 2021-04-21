90bfeea50bd1aebdedb38d55f56067533227ab62
Marcus Bianchi <82729031+CmdBianchi@users.noreply.github.com>
2021-04-21 10:36:13 -0300

    ADD FUNCTION Formula().

--- Exercício 03 -  Estrutura Sequencial/Program.cs
+++ Exercício 03 -  Estrutura Sequencial/Program.cs
@@ -4,24 +4,33 @@ namespace Exercício_03____Estrutura_Sequencial
 {
     class Program
     {
-        /// -------> START / MAIN 
+        /// -------> START / MAIN.F 
         /////////////////////////////////////////////////////////////////////////////////////////////
         static void Main(string[] args){
             Msg();
-
-
+            Formula();
         }
-
         /// -------> FUNCTIONS
         /////////////////////////////////////////////////////////////////////////////////////////////      
         static void Msg() {
-            Console.WriteLine("Diferença do produto de A e B pelo produto de C e D ");
-        } 
+            Console.WriteLine("Calculo da diferença do produto de A e B pelo produto de C e D ");
+        }
+        /*------------------------------------------------------------------------------------------*/
+        static void Formula() {
+            int a, b, c, d, diferenca;
 
+            Console.Write("Digite o valor de A: ");
+            a = int.Parse(Console.ReadLine());
+            Console.Write("Digite o valor de B: ");
+            b = int.Parse(Console.ReadLine());
+            Console.Write("Digite o valor de C: ");
+            c = int.Parse(Console.ReadLine());
+            Console.Write("Digite o valor de D: ");
+            d = int.Parse(Console.ReadLine());
 
-
-
-
+            diferenca = (a * b) - (c * d);
+            Console.Write("A Diferença é: " +diferenca);
+        }
         /////////////////////////////////////////////////////////////////////////////////////////////
         /// -------> END
     }
